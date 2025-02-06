405673d3ae942362ccce5279dcee2b4038cfe50f
graysen-a <graysen.aldridge@gmail.com>
2025-02-05 20:13:16 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈 Welcome to Bear's Adventure!")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+  
